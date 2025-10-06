54e057d5d2720ea0762332013f333242c30a6f78
Admin <Admin@DESKTOP-8URQBFL>
2025-10-06 17:37:08 +0500

    Map in progress

--- AssociativeContainers/main.cpp
+++ AssociativeContainers/main.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include<fstream>
 #include <string>
 #include <map>
 #include <list>
@@ -12,8 +13,8 @@ using  std::endl;
 
 #define STL_MAP
 
-template<typename T> void print_map(std::map< T, std::list<T>> cont);
-template<typename T> void print_map_range(std::map< T, std::list<T> > cont);
+template<typename T> void print_map(std::map< T, std::list<T>>& cont);
+template<typename T> void print_map_range(std::map< T, std::list<T>>& cont);
 
 void main()
 {
@@ -63,7 +64,7 @@ void main()
 #endif // STL_MAP
 }
 
-template<typename T> void print_map(std::map< T, std::list<T> > cont)
+template<typename T> void print_map(std::map< T, std::list<T>>& cont)
 {
 	for (typename std::map<T, std::list<T>>::iterator it = cont.begin(); it != cont.end(); ++it)
 	{
@@ -74,7 +75,7 @@ template<typename T> void print_map(std::map< T, std::list<T> > cont)
 		cout << endl;
 	}
 }
-template<typename T> void print_map_range(std::map< T, std::list<T> > cont)
+template<typename T> void print_map_range(std::map< T, std::list<T>>& cont)
 {
 	for (std::pair< T, std::list<T> > it : cont)
 	{
@@ -84,6 +85,10 @@ template<typename T> void print_map_range(std::map< T, std::list<T> > cont)
 		cout << endl;
 	}
 }
+
+
+
+
 
 
 //#include <iostream>
@@ -203,223 +208,4 @@ template<typename T> void print_map_range(std::map< T, std::list<T> > cont)
 //		cout << i->first << tab << i->second << endl;
 //	}
 //	cout << endl << endl;
-//}
-
-
-
-
-
-//﻿//PoliceLoadOptimisation
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<iostream>
-//#include<fstream>
-//#include<string>
-//#include<map>
-//#include<list>
-//#include<algorithm>
-//#include<Windows.h>
-//
-//#include"Crime.h"
-//
-//using std::cin;
-//using std::cout;
-//using std::endl;
-//
-//#define delimiter "\n-----------------------------------------------\n"
-//
-//void add(std::map<std::string, std::list<Crime>>& base);
-//void print(const std::map<std::string, std::list<Crime>>& base);
-//void print(const std::map<std::string, std::list<Crime>>& base, const std::string& licence_plate);
-//void print(const std::map<std::string, std::list<Crime>>& base, const std::string& first_plate, const std::string& last_plate);
-//void save(const std::map<std::string, std::list<Crime>>& base, const std::string& filename);
-//void load(std::map<std::string, std::list<Crime>>& base, const std::string& filename);
-//
-//void main()
-//{
-//	//setlocale(LC_ALL, "");
-//	SetConsoleCP(1251);
-//	SetConsoleOutputCP(1251);
-//	std::map<std::string, std::list<Crime>> base/* =
-//	{
-//		{"a777km", {Crime(1, "улица Ленина"), Crime(2, "ул. Фрунзе")}},
-//		{"a557ab", {Crime(5, "ул. Космонавтов")}},
-//		{"a001a", {Crime(4, "перекресток Ленина и Октябрьской"), Crime(3, "ул. Октябрьская")}}
-//	}*/;
-//	load(base, "base.txt");
-//	load(base, "base.txt");
-//	load(base, "base.txt");
-//	print(base);
-//
-//	/*std::string licence_plate;
-//	cout << "Введите номер транпортного средства: "; cin >> licence_plate;
-//	print(base, licence_plate);*/
-//
-//	std::string first_plate, last_plate;
-//	cout << "Введите начальный номерной знак: "; cin >> first_plate;
-//	cout << "Введите конечный номерной знак: "; cin >> last_plate;
-//	print(base, first_plate, last_plate);
-//	//save(base, "base.txt");
-//	//add(base);
-//	//print(base);
-//}
-//
-//void add(std::map<std::string, std::list<Crime>>& base)
-//{
-//	std::string licence_plate;
-//	int id;
-//	std::string place;
-//	cout << "Введите номер автомобиля: "; cin >> licence_plate;
-//	cout << "Введите правонарушение (номер статьи): "; cin >> id;
-//	cout << "Введите место проишествия: ";
-//	cin.clear();
-//	cin.ignore();
-//	std::getline(cin, place);
-//	//base[licence_plate].push_back(Crime(id, place));
-//	std::map<std::string, std::list<Crime>>::iterator it = base.find(licence_plate);
-//	if (it != base.end())
-//	{
-//		it->second.push_back(Crime(id, place));
-//	}
-//	else
-//	{
-//		base.insert(std::pair<std::string, std::list<Crime>>(licence_plate, { Crime(id, place) }));
-//	}
-//}
-//void print(const std::map<std::string, std::list<Crime>>& base)
-//{
-//	for (std::map<std::string, std::list<Crime>>::const_iterator it = base.begin(); it != base.end(); ++it)
-//	{
-//		//cout << std::left;
-//		cout << it->first + ":\n";
-//		for (std::list<Crime>::const_iterator jt = it->second.begin(); jt != it->second.end(); ++jt)
-//		{
-//			cout << "\t" << *jt << ";\n";
-//		}
-//		//cout << "\n-----------------------------------------------\n";
-//		cout << delimiter;
-//	}
-//}
-//void print(const std::map<std::string, std::list<Crime>>& base, const std::string& licence_plate)
-//{
-//	try
-//	{
-//		cout << licence_plate << ":\n";
-//		for (
-//			std::list<Crime>::const_iterator it = base.at(licence_plate).begin();
-//			it != base.at(licence_plate).end();
-//			++it
-//			)
-//			cout << *it << endl;
-//	}
-//	/*catch (const std::exception& e)
-//	{
-//		std::cerr << e.what() << endl;
-//	}*/
-//	catch (...)
-//	{
-//		std::cerr << "В базе нет такого номера" << endl;
-//	}
-//}
-//void print(std::map<std::string, std::list<Crime>>& base, const std::string& first_plate, const std::string& last_plate)
-//{
-//	for (
-//		std::map<std::string, std::list<Crime>>::iterator it = base.lower_bound(first_plate);
-//		it != base.upper_bound(last_plate);
-//		++it
-//		)
-//	{
-//		cout << it->first << ":\n";
-//		for (std::list<Crime>::const_iterator jt = it->second.begin(); jt != it->second.end(); ++jt)
-//		{
-//			cout << *jt << endl;
-//		}
-//		cout << delimiter;
-//	}
-//}
-//void save(const std::map<std::string, std::list<Crime>>& base, const std::string& filename)
-//{
-//	std::ofstream fout(filename);
-//	for (std::pair<std::string, std::list<Crime>> i : base)
-//	{
-//		fout << i.first << ":\t";
-//		for (Crime j : i.second)
-//		{
-//			fout << j << ", ";
-//		}
-//		fout.seekp(-2, std::ios::cur);	//сдвигаем курсор на 2 позиции влево
-//		fout << ";\n";
-//	}
-//	fout.close();
-//	std::string command = std::string("start notepad ") + filename;
-//	system(command.c_str());
-//	//c_str() возвращает C-string (NULL Terminated Line)
-//}
-//void load(std::map<std::string, std::list<Crime>>& base, const std::string& filename)
-//{
-//	std::ifstream fin(filename);
-//	if (fin.is_open())
-//	{
-//		while (!fin.eof())
-//		{
-//			std::string licence_plate;
-//			int id;
-//			std::string place;
-//			std::string crimes;
-//			std::getline(fin, licence_plate, ':');
-//			std::getline(fin, crimes);
-//			if (crimes.empty())continue;
-//
-//			/*if (crimes.find(',') != std::string::npos)
-//			{
-//				for (int start = 0, end = crimes.find(','); end != std::string::npos; start = end)
-//				{
-//					end = crimes.find(',', start + 1);
-//					place = crimes.substr(start + 1, end);//TODO: проверить с большим количеством нарушений
-//					//https://www.cplusplus.com/reference/string/string/substr/
-//					place.erase(0, place.find_first_not_of(' '));
-//					id = std::stoi(place.substr(0, place.find_first_of(' ')));
-//					place.erase(0, place.find_first_of(' ') + 1);
-//					base[licence_plate].push_back(Crime(id, place));
-//				}
-//			}
-//			else
-//			{
-//				id = std::stoi(crimes.substr(0, crimes.find_first_of(' ')));
-//				crimes.erase(0, crimes.find_first_of(' '));
-//				base[licence_plate].push_back(Crime(id, crimes));
-//			}*/
-//
-//			char* sz_crimes = new char[crimes.size() + 1] {};
-//			std::strcpy(sz_crimes, crimes.c_str());
-//			char sz_delimiters[] = ",;";
-//			for (char* pch = strtok(sz_crimes, sz_delimiters); pch; pch = strtok(NULL, sz_delimiters))
-//			{
-//				//atoi() - ASCII-string to int
-//				while (*pch == ' ')pch++;//Пока строка начинается с символа ' ', смещаем начало строки на 1 символ враво.
-//				id = std::atoi(pch);	 //Берем номер правонарушения, преобразуем его в целочисленный формат (int), и сохраняем в id.
-//				pch = std::strchr(pch, ' ') + 1;//Для того чтобы убрать id из строки, находим первый пробел в строке, 
-//				//и смещаем начало строки на символ, следующий за пробелом.
-//				/*if (
-//					std::find(base.at(licence_plate).begin(), base.at(licence_plate).end(), Crime(id, pch))
-//					== base.at(licence_plate).end()
-//					)*/
-//				if
-//					(
-//						std::find
-//						(
-//							base[licence_plate].begin(),
-//							base[licence_plate].end(),
-//							Crime(id, pch)
-//						) == base[licence_plate].end()
-//						)
-//					base[licence_plate].push_back(Crime(id, pch));
-//			}
-//			delete[] sz_crimes;
-//		}
-//		fin.close();
-//	}
-//	else
-//	{
-//		std::cerr << "Error: file not found" << endl;
-//	}
 //}
---
+++ PoliceBase/main.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <list>
+#include <map>
+
+using namespace std;
+
+#define tab			 "\t"
+#define delimiter	 "\n-----------------------------------------------------\n"
+
+class Offense;
+void print_base(const std::map<std::string, std::list<Offense>>& cont);
+void add_to_list(std::map<std::string, std::list<Offense>>& cont);
+
+class Offense
+{
+	std::string protocol;
+	std::string addressOffense;
+public:
+	const std::string& get_num_prot() const
+	{
+		return protocol;
+	}
+	const std::string& get_address() const
+	{
+		return addressOffense;
+	}
+	Offense(const std::string& protocol, const std::string& addressOffense) : protocol(protocol), addressOffense(addressOffense) {}
+	~Offense() {};
+};
+
+static const std::map<std::string, std::string> OffenseList =
+{
+	{"AP0001", "превышение скорости"},
+	{"AP0002", "проезд на красный"},
+	{"AP0003", "пересечение сплошной"},
+	{"AP0004", "драка с полицейским"},
+};
+
+std::ostream& operator<<(std::ostream& os, const Offense& obj)
+{
+	return os << obj.get_num_prot() << tab << OffenseList.at(obj.get_num_prot()) << tab << obj.get_address() << endl;
+}
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	std::map<std::string, std::list<Offense>> base =
+	{
+			{"b007nd", {Offense("AP0004", "пр. Ћенина 69")}},
+			{"a123nc", {Offense("AP0001", "пр. Ћенина 69"), Offense("AP0003", "ул. Ёнтузиастов"), Offense("AP0002", "ул. Ёнтузиастов")}}
+	};
+	print_base(base);
+	cout << endl;
+	//add_to_list(base);
+	//print_base(base);
+	cout << endl;
+
+}
+
+void print_base(const std::map< std::string, std::list<Offense>>& cont)
+{
+	for (std::map< std::string, std::list<Offense>>::const_iterator it = cont.begin(); it != cont.end(); ++it)
+	{
+		cout.width(15);
+		cout << it->first << ":" << tab;
+		for (std::list<Offense>::const_iterator sec_it = it->second.begin(); sec_it != it->second.end(); ++sec_it)
+		{
+			cout.width(15);
+			cout << *sec_it << tab;
+		}
+		cout << endl;
+	}
+}
+
+void add_to_list (std::map<std::string, std::list<Offense>>& cont)
+{
+	std::string number_automobile;
+	std::string protocol;
+	std::string addressOffense;
+	cout << "¬ведите номер автомобил€: "; cin >> number_automobile;
+	cout << "¬ведите правонарушение (номер статьи): "; cin >> protocol;
+	cout << "¬ведите место проишестви€: ";
+	cin.clear();
+	cin.ignore();
+	std::getline(cin, addressOffense);
+	cont[number_automobile].push_back(Offense(protocol, addressOffense));
+}
